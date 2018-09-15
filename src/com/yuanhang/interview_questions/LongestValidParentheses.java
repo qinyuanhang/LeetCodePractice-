dc888a6754eadb5dd47645ee5dd80a6d8aabb622
qinyuanhang <qinyuanhang1234@gmail.com>
2018-09-15 16:37:20 -0700

    update LongestValideParenttheses
    add test cases and constructor

--- src/com/yuanhang/interview_questions/LongestValidParentheses.java
+++ src/com/yuanhang/interview_questions/LongestValidParentheses.java
@@ -2,34 +2,64 @@ package com.yuanhang.interview_questions;
 
 import java.util.Stack;
 
+import com.sun.istack.internal.logging.Logger;
+import com.sun.org.apache.bcel.internal.generic.RETURN;
+
+
+
+/**
+ * Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+
+Input: "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()"
+Example 2:
+
+Input: ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()"
+ * 
+ * */
+
 public class LongestValidParentheses {
+	
+	public LongestValidParentheses(){
+		testLongestValidParentheses();
+	}
+	
 	public int longestValidParentheses(String s) {
-        int res = 0;
-        if(s.length() == 0){
-            return res;
-        }        
-        Stack<Integer> stack = new Stack<Integer>();
-        int max = 0;
+        int res = 0, tempL = 0;       
+        Stack<Integer> positionStack = new Stack<>();
+        positionStack.push(-1);
         
         for(int i = 0; i < s.length(); i++){
             if(s.charAt(i) == '('){
-                stack.push(i);
+                positionStack.push(i);
             }else{
-                if(stack.empty()){
-                    max = 0;
-                }else{
-                    int matchL = i - stack.pop() + 1;
-                    if(stack.empty()){
-                        max += matchL;
-                        matchL = max;
-                    }else{
-                        matchL = i - stack.peek();
-                    }
-                      res = Math.max(matchL,res);
-                }
+               positionStack.pop();
+               if (positionStack.empty()) {
+            	   positionStack.push(i);
+               } else {
+            	   res = Math.max(res, i - positionStack.peek());           	   
+               }
             }
           
         }
         return res;
     }
+	
+	
+	public void testLongestValidParentheses() {
+		String [] testStrings = {
+				"()()()()(((())))",
+				"(()()()()()()",
+				"(()(()()()"
+		};
+		
+		for (String test : testStrings){
+			System.out.println(test + " answer is: " + longestValidParentheses(test));
+		}
+	}
 }
